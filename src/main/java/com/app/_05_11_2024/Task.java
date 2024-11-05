406f80a5f66aa350c7fd37d3f789b5cb3e494a78
shonmikl <mikhailegorov676@gmail.com>
2024-11-05 14:25:25 +0100

    task

--- src/main/java/com/app/_05_11_2024/Task.java
+++ src/main/java/com/app/_05_11_2024/Task.java
@@ -1,5 +1,12 @@
 package com.app._05_11_2024;
 
+import com.github.javafaker.Faker;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Task {
     /**
      * Person
@@ -27,4 +34,38 @@ public class Task {
      * 2 = TreeSet = name, surname, age, Address
      * 3 = LinkedList age > param
      */
+
+
+}
+
+@ToString
+class Person1 {
+    String name;
+    String surName;
+
+    public Person1(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    public Person1() {
+    }
+
+    public List<Person1> gen() {
+        List<Person1> l = new ArrayList<>();
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 5; i++) {
+            l.add(new Person1(faker.name().firstName(), faker.name().lastName()));
+        }
+
+        System.out.println(l);
+
+        return l;
+    }
+
+    public static void main(String[] args) {
+        new Person1().gen();
+    }
+
 }
